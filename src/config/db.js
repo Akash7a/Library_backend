53f73ba566ef3cc31f7e99932a261f7ee1f5656a
Akash Kumar <akashkumar914a@gmail.com>
2024-12-28 15:52:01 +0530

    changed the database from mongodb local to mongodb atlas

--- src/config/db.js
+++ src/config/db.js
@@ -1,11 +1,16 @@
 import mongoose from "mongoose";
 import { DB_NAME } from "../constant.js";
+import dotenv from "dotenv";
+
+dotenv.config({
+    path:"../.env"
+});
 
 export const connectDB = async () => {
     try {
-        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}${DB_NAME}?retryWrites=true&w=majority`);
         console.log(`MongoDB connection HOST:: ${connectionInstance.connection.host}`);
     } catch (error) {
-        console.error(`MongoDB connection failed Error ${error}`);
+        console.error(`MongoDB connection failed: ${error.message}`);
     }
-}
+};
